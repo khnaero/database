fbf3232b5a820240c3ccc822157c0ceefc088e00
Khoi Nguyen <khnaero@gmail.com>
2014-09-16 19:33:06 -0500

    Add database1.rb

--- database1.rb
+++ database1.rb
@@ -1,10 +1,17 @@
 foo, bar, baz = ARGV
 if foo == "store"
-	if baz == "hi"
-		puts "hi"
-	else
-		puts "bye bye"
-	end
-else
-	puts "hi"
+	newfile = open("key.txt", 'a')
+	newfile.puts("#{bar}, #{baz}")
+	puts "#{baz}"
+	newfile.close # get error if put at end of file
 end
+
+if foo == "retrieve"
+ 	stuff = Hash[*File.read("key.txt").split(/[, \n]+/)]
+ 	puts stuff[bar]
+end
+
+
+# need help with 4th argument
+# arrays have some sort of uniq method; is there one for hash?
+
